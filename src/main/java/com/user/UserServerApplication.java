7c36c976a69f2c4302b130d721276be673a58f77
stepan.sichkar <stepan.sichkar@arcadia.spb.ru>
2017-06-27 18:59:29 +0300

    Added CORS headers

--- src/main/java/com/user/UserServerApplication.java
+++ src/main/java/com/user/UserServerApplication.java
@@ -1,26 +1,17 @@
 package com.user;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class UserServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServerApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/users").allowedOrigins("http://localhost:9000");
-			}
-		};
 	}
 }
